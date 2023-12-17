42c35b5aea20ca4f64447b4ee2facc2300d3dcf2
JHart96 <jordan.da.hart@gmail.com>
2023-12-17 21:58:07 +0000

    Updated config

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,8 +33,8 @@ export default function Home() {
     const { siteConfig } = useDocusaurusContext();
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />"
+            title={`${siteConfig.title}`}
+            description="CausaDB is a cloud-native platform for building, managing, and deploying causal AI."
         >
             <HomepageHeader />
             <main>
